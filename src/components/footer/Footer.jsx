56c48ff8e46d4d92ecd6ee8e941b786870551d3a
Marcin Winiarski <winiardesign@gmail.com>
2023-06-19 21:01:05 +0200

    add UX

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from "./page.module.css";
+import Image from 'next/image';
 
 function Footer() {
   return (
     <div className={styles.container}>
       <div>@2023 Created by Marcin </div>
-      <div>
-        
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="facebook" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="instagram" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="twitter" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="youtube" />
       </div>
     </div>
   )
