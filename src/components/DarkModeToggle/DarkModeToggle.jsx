0b6c66f0e0ffffb5ac861e09b9d5e2b27a222c4c
Marcin Winiarski <winiardesign@gmail.com>
2023-06-21 20:12:26 +0200

    add dark / light mode

---
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React, { useContext } from 'react';
+import styles from './darkModeToggle.module.css';
+import { ThemeContext } from '@/context/ThemeContex';
+
+function DarkModeToggle() {
+    const {toggle, mode} = useContext(ThemeContext);
+  return (
+     <div className={styles.container} onClick={toggle}>
+        <div className={styles.icon}>S</div>
+        <div className={styles.icon}>M</div>
+         <div className={styles.ball} style={mode === "light" ? {left: "2px"} : {right: "2px"}}/>
+    </div>
+  )
+}
+
+export default DarkModeToggle
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import React from 'react';
 import styles from './page.module.css';
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
 
 const links = [
     {
@@ -41,6 +42,7 @@ function Navbar() {
     <div className={styles.container}>
         <Link href="/" className={styles.logo}>Lamamia</Link>
         <div className={styles.links}>
+        <DarkModeToggle />
             {links.map( link => (
                 <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
             ))}
